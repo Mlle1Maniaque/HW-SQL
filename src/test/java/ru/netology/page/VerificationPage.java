fe52548889ffaa6b6e706dd9a927abab2f78b08b
Mlle1Maniaque <127653126+Mlle1Maniaque@users.noreply.github.com>
2023-07-21 10:04:27 +0300

    update

--- src/test/java/ru/netology/page/VerificationPage.java
+++ src/test/java/ru/netology/page/VerificationPage.java
@@ -9,7 +9,7 @@ import static com.codeborne.selenide.Selenide.$;
 public class VerificationPage {
     private SelenideElement code = $("[data-test-id=code] input");
     private SelenideElement verifyButton = $("[data-test-id=action-verify]");
-    private SelenideElement error = $(".notification__content");
+    private SelenideElement error = $("[data-test-id=error-notification]");
 
     public void validVerify(String VerifyCode) {
         verify(VerifyCode);
@@ -21,7 +21,6 @@ public class VerificationPage {
     }
 
     public void getError() {
-
         error.shouldBe(visible);
         error.shouldHave(text("Неверно указан код! Попробуйте ещё раз"));
     }
